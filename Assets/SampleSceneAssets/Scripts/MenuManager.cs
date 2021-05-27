d35629431789ddae6faa096de62cd9e92f2d6fde
Barbas_Oyun <yoan.lechevalier@gmail.com>
2021-05-27 14:15:21 +0200

    Reset

--- Assets/SampleSceneAssets/Scripts/MenuManager.cs
+++ Assets/SampleSceneAssets/Scripts/MenuManager.cs
@@ -22,12 +22,33 @@ public class MenuManager : MonoBehaviour
     public GameObject savePanel;
     public GameObject languagePanel;
 
-    [Header("Buttons")]
+    [Header("MenuButtons")]
     public Button resumeButton;
     public Button parameterButton;
     public Button saveButton;
     public Button inputsButton;
     public Button languagesButton;
+
+    [Header("RebindingButtons")] 
+    public RectTransform rebindingInteractKeyboard;
+    public RectTransform resetInteractKeyboard;
+    public RectTransform rebindingInteractController;
+    public RectTransform resetInteractController;
+
+    public RectTransform rebindingMoveKeyboard;
+    public RectTransform resetMoveKeyboard;
+    public RectTransform rebindingMoveController;
+    public RectTransform resetMoveController;
+    
+    public RectTransform rebindingJumpKeyboard;
+    public RectTransform resetJumpKeyboard;
+    public RectTransform rebindingJumpController;
+    public RectTransform resetJumpController;
+    
+    public RectTransform rebindingPoseTorchKeyboard;
+    public RectTransform resetPoseTorchKeyboard;
+    public RectTransform rebindingPoseTorchController;
+    public RectTransform resetPoseTorchController;
 
     [Header("Language Buttons")]
     public Button englishButton;
@@ -39,7 +60,7 @@ public class MenuManager : MonoBehaviour
 
     void Awake()
     {
-        Gears.gears.playerInput.actions["Interact"].performed += context => Debug.Log("Interaction");
+        //Gears.gears.playerInput.actions["Interact"].performed += context => Debug.Log("Interaction");
         Gears.gears.playerInput.actions["Escape"].performed += context => Menu();
         Gears.gears.playerInput.actions["EscapeMenu"].performed += context => Menu();
     }
@@ -51,12 +72,17 @@ public class MenuManager : MonoBehaviour
         PanelMaps = new[] {new PanelMap("PauseMap", 
             new [, ] {{resumeButton.GetComponent<RectTransform>(), parameterButton.GetComponent<RectTransform>(), saveButton.GetComponent<RectTransform>()}}, 
             new Vector2Int(0, 0)),  
-            new PanelMap("ParameterMap", new [,] {{languagesButton.GetComponent<RectTransform>(), backButton.GetComponent<RectTransform>()}}, 
-                new Vector2Int(0, 0)), 
+            new PanelMap("ParameterMap", new [,] {{backButton.GetComponent<RectTransform>(), languagesButton.GetComponent<RectTransform>(), 
+                    inputsButton.GetComponent<RectTransform>()}}, new Vector2Int(0, 0)), 
             new PanelMap("LanguageMap", new [,] {{englishButton.GetComponent<RectTransform>(), frenchButton.GetComponent<RectTransform>(), 
                     backButton.GetComponent<RectTransform>()}}, 
-                new Vector2Int(0, 0)), 
-            new PanelMap("Save Panel", new [,] {{backButton.GetComponent<RectTransform>()}}, new Vector2Int(0, 0)), 
+                new Vector2Int(0, 0)), CreatePanelMap(savePanel),
+            //new PanelMap("SaveMap", new [,] {{backButton.GetComponent<RectTransform>()}}, new Vector2Int(0, 0)), 
+            new PanelMap("RebindingMap", new [,] {
+                {backButton.GetComponent<RectTransform>(), rebindingInteractKeyboard, rebindingMoveKeyboard, rebindingJumpKeyboard, rebindingPoseTorchKeyboard}, 
+                {backButton.GetComponent<RectTransform>(), resetInteractKeyboard, resetMoveKeyboard, resetJumpKeyboard, resetPoseTorchKeyboard}, 
+                {backButton.GetComponent<RectTransform>(), rebindingInteractController, rebindingMoveController, rebindingJumpController, rebindingPoseTorchController}, 
+                {backButton.GetComponent<RectTransform>(), resetInteractController, resetMoveController, resetJumpController, resetPoseTorchController}}, new Vector2Int(0, 0))
         };
 
         resumeButton?.onClick.AddListener(Menu);
@@ -72,7 +98,7 @@ public class MenuManager : MonoBehaviour
         //Go to Inputs panel when you click inputs button and setup the back button to go back to parameter panel
         inputsButton?.onClick.AddListener(() => GoToPanel(inputsPanel, inputsButton.transform.parent.gameObject, 
             () => GoToPanel(parametersPanel, parameterButton.transform.parent.gameObject, 
-                () => GoToPanel(menuPanel, null, null, PanelMaps[0], false), PanelMaps[1])));
+                () => GoToPanel(menuPanel, null, null, PanelMaps[0], false), PanelMaps[1]), PanelMaps[4]));
         
         //Go to language panel when you click language button and setup the back button to go back to parameter panel
         languagesButton?.onClick.AddListener(() => GoToPanel(languagePanel, languagePanel.transform.parent.gameObject, 
@@ -90,7 +116,9 @@ public class MenuManager : MonoBehaviour
     {
         
     }
-    
+
+    #region MenFunc
+
     public void Menu()
     {
         if (menu)
@@ -125,6 +153,7 @@ public class MenuManager : MonoBehaviour
             currentMap = panelMap;
             selection.GetComponent<RectTransform>().position = panelMap.map[panelMap.startPos.x, panelMap.startPos.y].position;
             selection.posOnMap = currentMap.startPos;
+            //Debug.Log(panelMap.mapName);
         }
     }
 
@@ -158,40 +187,137 @@ public class MenuManager : MonoBehaviour
         
         button.GetComponent<Image>().color = button.colors.normalColor;
     }
+    
+    #endregion
 
-    public IInteractable Interact(float interactionRadius)
+    public PanelMap CreatePanelMap(GameObject panel)
     {
-        RaycastHit[] hits = Physics.SphereCastAll(transform.position, interactionRadius, gameObject.transform.forward, 
-            Mathf.Infinity, Gears.gears.interactionLayer);
+        List<GameObject> allChilds = GetAllChilds(panel);
+        List<RectTransform> recter = new List<RectTransform>();
 
-        Dictionary<IInteractable, GameObject> interactable = new Dictionary<IInteractable, GameObject>();
-        
-        List<IInteractable> interact = new List<IInteractable>();
-        
-        for (int i = 0; i < hits.Length; i++)
+        foreach(var child in allChilds)
         {
-            if (hits[i].collider.TryGetComponent(out IInteractable interaction))
+            if (child.TryGetComponent(out Button button))
             {
-                interactable.Add(interaction, hits[i].collider.gameObject);
-                interact.Add(interaction);
+                recter.Add(button.GetComponent<RectTransform>());
             }
         }
 
-        IInteractable interactionF = null;
-        float distance = Mathf.Infinity;
+        List<List<RectTransform>> lists = new List<List<RectTransform>>();
+        int currentListIndex = -1;
         
-        for (int i = 0; i < interactable.Count; i++)
+        int column = 0;
+        int row = 0;
+        float posX = Mathf.Infinity;
+
+        for (int i = 0; i < recter.Count; i++)
         {
-            if (Vector3.Distance(transform.position, interactable[interact[i]].transform.position) < distance)
+            if (recter[i].position.x != posX)
             {
-                distance = Vector3.Distance(transform.position, interactable[interact[i]].transform.position);
-                interactionF = interact[i];
+                posX = recter[i].position.x;
+                lists.Add(new List<RectTransform>());
+                currentListIndex++;
+                
+                //Debug.Log($"add end list1 : {recter[i].gameObject.name}");
+                lists[currentListIndex].Add(recter[i]);
+            }
+            else
+            {
+                //order the row lists
+                /*if (lists[currentListIndex].Count > 0)
+                {
+                    for (int j = 0; j < lists[currentListIndex].Count; j++)
+                    {
+                        if (lists[currentListIndex][j].position.y > recter[i].position.y)
+                        {
+                            Debug.Log($"{j} {lists[currentListIndex][j].position.y} > {recter[i].position.y}");
+                            lists[currentListIndex].Insert(j, recter[i]);
+                            break;
+                        }
+                        
+                        if (j == lists[currentListIndex].Count - 1)
+                        {
+                            Debug.Log($"end");
+                            lists[currentListIndex].Add(recter[i]);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.Log($"count 0");
+                    lists[currentListIndex].Add(recter[i]);
+                }*/
+                
+                //Debug.Log($"add end list : {recter[i].gameObject.name}");
+                lists[currentListIndex].Add(recter[i]);
             }
         }
         
-        interactionF?.Interact();
+        //backButton setup
+        lists[0].Insert(0, null);
 
-        return interactionF;
+        column = lists.Count;
+        int maxLength = 0;
+
+        foreach (var list in lists)
+        {
+            if (list.Count > maxLength)
+            {
+                maxLength = list.Count;
+            }
+        }
+
+        row = maxLength;
+        
+        RectTransform [,] rectTransform = new RectTransform[column, row];
+        rectTransform[0, 0] = backButton.GetComponent<RectTransform>();
+
+        for (int i = 0; i < column; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                if (!(i == 0 && j == 0))
+                {
+                    if (j < lists[i].Count)
+                    {
+                        rectTransform[i, j] = lists[i][j];
+                    }
+                    else
+                    {
+                        rectTransform[i, j] = null;
+                    }
+                }
+            }
+        }
+
+        PanelMap panelMap = new PanelMap(panel.name, rectTransform, new Vector2Int(0, 0));
+        return panelMap;
+    }
+
+    public static List<GameObject> GetAllChilds(GameObject go)
+    {
+        List<GameObject> allChilds = new List<GameObject>();
+
+        for (int i = 0; i < go.transform.childCount; i++)
+        {
+            allChilds.Add(go.transform.GetChild(i).gameObject);
+
+            if (go.transform.GetChild(i).childCount > 0)
+            {
+                List<GameObject> g = GetAllChilds(go.transform.GetChild(i).gameObject);
+
+                foreach (var gj in g)
+                {
+                    if (!allChilds.Contains(gj))
+                    {
+                        allChilds.Add(gj);
+                    }
+                }
+            }
+        }
+        
+        return allChilds;
     }
 }
 
